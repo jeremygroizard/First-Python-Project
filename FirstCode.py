0a6187aff6fd63f5e4fd0238c43572f9afe960a8
jeremygroizard <jeremgroizard@gmail.com>
2023-12-24 23:13:20 +0700

    push from la gacilly

--- FirstCode.py
+++ FirstCode.py
@@ -1,1 +1,15 @@
-print("Hello word !")
+print("EPAL wood pallet cost calculator")
+print("For pallet made from pine maritime wood")
+
+print("0,17 m3 of maritime pine wood is needed")
+Maritime_pine_wood_price = float(input("Please enter the current market price of 1 m3 maritime pine wood"))
+Pallet_wood_volume = 0.17
+
+Total_wood_price = Maritime_pine_wood_price * Pallet_wood_volume
+print(f"Total wood price is {Total_wood_price} Euros")
+
+print("42 pieces of M5_5x90 threaded nails") 
+M5_5x90_threaded_nails_price = float(input("please enter the price with all taxes included : "))
+
+print("18 pieces of M5_5x38 threaded nails")
+M5_5x38_threaded_nails_price = float(input("Please enter the price with all taxes included : "))
